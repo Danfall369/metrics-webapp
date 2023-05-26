8f02867ffc052aea4f10a0efa0391827c182ad4d
Daniel Falcon <danfall963@gmail.com>
2023-05-26 01:15:37 -0400

    search bar totaly working by filter

--- src/components/routes/Products.jsx
+++ src/components/routes/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
@@ -8,24 +8,48 @@ function Header() {
   const { products, loading } = useSelector((state) => state.products);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]);
 
-  if (loading) {
-    return <h2>Loading...</h2>;
-  }
+  const filterProducts = useCallback(() => {
+    const filtered = products.filter((item) => {
+      const titleMatch = item.title.toLowerCase().includes(searchQuery.toLowerCase());
+      const priceMatch = item.price.toString().includes(searchQuery);
+      const categoryMatch = item.category.toLowerCase().includes(searchQuery.toLowerCase());
+
+      return titleMatch || priceMatch || categoryMatch;
+    });
+
+    setFilteredProducts(filtered);
+  }, [products, searchQuery]);
+
+  useEffect(() => {
+    filterProducts();
+  }, [filterProducts]);
 
   const onHandleClick = (id) => {
     dispatch(addDetails(id));
     navigate(`/Details/${id}`);
   };
 
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <header>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search..."
+      />
       <ul>
-        {products.map((item) => (
+        {filteredProducts.map((item) => (
           <li key={item.id}>
             <Card style={{ width: '18rem' }}>
               <Card.Body>
